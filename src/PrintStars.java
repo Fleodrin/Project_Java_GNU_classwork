d3ae1cb25972e10d495e99b9c7181923a68ff426
Vladimir Krivitskii <99479120+Fleodrin@users.noreply.github.com>
2023-03-20 13:34:52 +0900

    PrintStars@v2

--- src/PrintStars.java
+++ src/PrintStars.java
@@ -1,7 +1,7 @@
 public class PrintStars {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 10; j++) {
+            for (int j = 5 - i; j > 0; j--) {
                 System.out.print("*");
             }
             System.out.println();
