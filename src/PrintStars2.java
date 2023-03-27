ed59f398790a734e0d9d8887eb8ae6926a6f2dda
Vladimir Krivitskii <99479120+Fleodrin@users.noreply.github.com>
2023-03-27 12:20:49 +0900

    Add minitest for 4rd week
    27.03.2023

---
+++ src/PrintStars2.java
@@ -0,0 +1,17 @@
+public class PrintStars2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
