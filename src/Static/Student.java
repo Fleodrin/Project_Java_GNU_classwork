1a87522d50c7130cb57e0ad605257f31f53a6125
fleodrin <faret224@gmail.com>
2023-05-01 22:49:46 +0900

    Remove unused definitions

--- src/Static/Student.java
+++ src/Static/Student.java
@@ -10,10 +10,6 @@ public class Student {
 
 class test {
     public static void main(String[] args) {
-        Student std1 = new Student();
-        Student std2 = new Student();
-        Student std3 = new Student();
-
         System.out.println(Student.numStudentRegistered);
     }
 }
