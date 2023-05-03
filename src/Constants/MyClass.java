f70a32355b62401615b5418f82703699898c2a8a
Vladimir Krivitskii <99479120+Fleodrin@users.noreply.github.com>
2023-05-03 09:28:06 +0900

    Create constant variables

---
+++ src/Constants/MyClass.java
@@ -0,0 +1,14 @@
+package Constants;
+
+public class MyClass {
+    public static final int COUNT = 0;
+    public static final boolean SWITCHED_ON = false;
+
+    public static void main(String[] args) {
+        System.out.println(COUNT);
+
+//        COUNT = 10;
+//        SWITCHED_ON = true;
+        // ERROR ^
+    }
+}
