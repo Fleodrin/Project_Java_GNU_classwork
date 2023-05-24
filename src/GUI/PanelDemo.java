eb780480af612c4a6ec0c23ad8b52a479fdb929c
Vladimir Krivitskii <99479120+Fleodrin@users.noreply.github.com>
2023-05-24 22:41:45 +0900

    GUI

---
+++ src/GUI/PanelDemo.java
@@ -0,0 +1,24 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelDemo extends JFrame {
+    JPanel p1 = new JPanel();
+    JPanel p2 = new JPanel();
+
+    public PanelDemo() {
+        super("Panel demo");
+        p1.add(new JButton("JButton"));
+        p1.add(new JButton("JButton"));
+        p2.add(new JTextArea("JTextArea"));
+        Container cp = getContentPane();
+        cp.add(p1, BorderLayout.NORTH);
+        cp.add(p2, BorderLayout.CENTER);
+        pack();
+    }
+
+    public static void main(String[] args) {
+        PanelDemo PanelDemo = new PanelDemo();
+    }
+}
