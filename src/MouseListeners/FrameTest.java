8cf7570f199c8348ad1e220f0ab0c7a70d2b4cee
Vladimir Krivitskii <99479120+Fleodrin@users.noreply.github.com>
2023-06-05 13:26:19 +0900

    Added MouseListeners

--- src/MouseListeners/FrameTest.java
+++ src/MouseListeners/FrameTest.java
@@ -1,2 +1,18 @@
-package MouseListeners;public class FrameTest {
+package MouseListeners;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FrameTest {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Frame Test");
+        f.setLayout(new FlowLayout());
+        JButton button1 = new JButton("press me");
+        MyListener myListener = new MyListener();
+        button1.addMouseListener(myListener);
+        f.add(button1);
+        f.setSize(300, 200);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
 }
--- src/MouseListeners/MyListener.java
+++ src/MouseListeners/MyListener.java
@@ -1,2 +1,31 @@
-package MouseListeners;public class MyListener {
+package MouseListeners;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MyListener implements MouseListener {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("Mouse Clicked");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("Mouse Pressed");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        System.out.println("Mouse Released");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        System.out.println("Mouse Entered");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        System.out.println("Mouse Exited");
+    }
 }
